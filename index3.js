ec6b9da03cff737540491597070927da91ab0024
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-09-23 00:52:22 +0530

    index3.js : we created the 'custom middleware' . explained in the comments.

--- index3.js
+++ index3.js
@@ -3,7 +3,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+
+// this is my "custom middleware"
+
+function logger(req, res, next) {
+    console.log("This it the logger function!")
+    console.log("Request Method : " + req.method);
+    console.log("Request url : " + req.url);
+
+    next();     // it is very important to add this at end.
+    // if we don't add this ... the end-point "/" won't reach. as the function never exits. & remain till #HERE
+}
+
+
+app.use(logger);      // #HERE 
 
 app.get("/", (req, res) => {
   res.send("Hello");
