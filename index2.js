07527a71e4f71940095cb44e2b82e8c8c09568a9
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-09-23 00:31:50 +0530

    we used the 'morgan' : the logging middleware to log the details on console.

--- index2.js
+++ index2.js
@@ -1,11 +1,20 @@
+// Morgan is used for "logging" type Middleware.
+
+import morgan from "morgan";          // npm i morgan;
 import express from "express";
+
 
 const app = express();
 const port = 3000;
 
+app.use(morgan("tiny"))
+// now we'll test the logging on the localhost with Postman.
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+// just got to default GET URL in Postman. & check here in CLI.(cmd)
+// CLI o/p : "GET / 200 5 - 7.551 ms"
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
