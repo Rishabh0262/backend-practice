265e8f42a4c331e1515d58edd9c732743fc5dba0
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-09-23 00:10:56 +0530

    WE've used 'bodypaser' as the 'pre-processor' type middleware. Rest the details have been added in comments in index1.js

---
+++ README.md
@@ -0,0 +1,7 @@
+## with initailly provided files. [commit 1]
+
+## On CMD : 
+
+    1. npm i
+
+    2. nodemon index1.js
--- index1.js
+++ index1.js
@@ -1,3 +1,7 @@
+// Body parser is of "pre-processor" type middleware.
+
+import bodyParser from "body-parser";         // We added this package 
+
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -7,8 +11,22 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  // console.log(__dirname + "/public/index.html")
+
   res.sendFile(__dirname + "/public/index.html");
 });
+
+
+// ".use()" : Mount a middleware using express.
+//  ".urlencoded({extended : true})" : to create a body for URL-encoded request like our form submission. 
+//  as we wanted the URL incoded DATA that is sent via POST from the form. 
+app.use(bodyParser.urlencoded({extended : true}))
+
+app.post("/submit", (req, res)=> {
+  console.log(req.body)
+})
+
+//  on Postman : we used the URL : localhost:3000/submit ... POST : data via  Body : x-www-form-urlencoded
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
