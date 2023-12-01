83e09e6fc7c3fd3d2848be8a710a8866e78c89c5
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-12-02 01:42:05 +0530

    I tried doing the task... but there seems an error in that.

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  var data = {
+    fName : '',
+    lName : "",
+    size : 0
+  }
+  res.render("index.ejs", data);
+  
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  var localFName = req.body["fName"]
+  var localLName = req.body["lName"]
+
+  var n = localFName.length + localLName.length
+  var data = {
+    fName : localFName,
+    lName : localLName,
+    size : n
+  }
+
+   
+
+  res.render('index.ejs', data)
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
